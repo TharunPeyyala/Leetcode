059758366c47684a0848c4e6f7a47f1a0931dbe4
TharunPeyyala <tharunpeyyala2906@gmail.com>
2025-08-27 23:01:23 +0530

    Update 0496-next-greater-element-i.java

--- Day8/0496-next-greater-element-i.java
+++ Day8/0496-next-greater-element-i.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
 
+        // Build a map from each element in nums2 to its next greater element
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        // Build the result for nums1 using the map
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return res;
+    }
+}
