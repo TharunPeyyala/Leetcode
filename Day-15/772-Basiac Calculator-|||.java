4f77b5542ac8b63594b2004a20086bd9a4271953
TharunPeyyala <tharunpeyyala2906@gmail.com>
2025-09-03 20:44:36 +0530

    Update 772-Basiac Calculator-|||.java

--- Day-15/772-Basiac Calculator-|||.java
+++ Day-15/772-Basiac Calculator-|||.java
@@ -1,12 +1,41 @@
-int startIndex=0;
-public int Calculate(String inputString){
-  Stack<integer> numberStack=new Stack();
-int number=0;
-char operator='+';
-while(startIndex<inputString.length()){
-  char charval=inputString.charAt(startIndex);
-  if(Character.isDigit(charValue)){
-    number=(number*10)+(charValue-'0');
-  }
-  if((!Character.isDigit(charValue)&&charValue!=' ') || (startIndex==inputString.length()-1)){
-    if(char
+class Solution {
+    public int calculate(String s) {
+        return helper(s, new int[]{0});
+    }
+
+    private int helper(String s, int[] indexRef) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int num = 0;
+        char sign = '+';
+        int n = s.length();
+        
+        while (indexRef[0] < n) {
+            char c = s.charAt(indexRef[0]);
+            indexRef[0]++;
+
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            else if (c == '(') {
+                num = helper(s, indexRef);
+            }
+
+            if ((!Character.isDigit(c) && c != ' ' && c != '(') || indexRef[0] == n) {
+                switch (sign) {
+                    case '+': stack.push(num); break;
+                    case '-': stack.push(-num); break;
+                    case '*': stack.push(stack.pop() * num); break;
+                    case '/': stack.push(stack.pop() / num); break;
+                }
+                sign = c;
+                num = 0;
+            }
+
+            if (c == ')') break;
+        }
+
+        int result = 0;
+        while (!stack.isEmpty()) result += stack.pop();
+        return result;
+    }
+}
