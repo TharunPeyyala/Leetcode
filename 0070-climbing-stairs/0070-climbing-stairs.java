fff822a95c6c18ab994de84a425bb8a7bb44d7d5
TharunPeyyala <tharunpeyyala2906@gmail.com>
2025-11-22 09:53:08 +0530

    Time: 0 ms (100%), Space: 42.1 MB (15.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==0 || n==1){
+            return 1;
+        }
+        int n1=1;
+        int n2=1;
+        for(int i=2;i<=n;i++){
+            int current=n1+n2;
+            n1=n2;
+            n2=current;
+        }
+        return n2;
+    }
+}
